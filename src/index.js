c62ab332f573bf444eac73985ce113a1c3761a12
조윤정 <hiyun20031@gmail.com>
2022-11-18 01:53:22 +0900

    vanilla js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+let count = 0;
+
+number.innerText = count;
+
+const updateText = () => {
+  number.innerText = count;
+};
+
+const handlePlus = () => {
+  count++;
+  updateText();
+};
+const handleMinus = () => {
+  count--;
+  updateText();
+};
+
+plus.addEventListener("click", handlePlus);
+minus.addEventListener("click", handleMinus);
