0f9136d3c15b64c31ae1b3603ce492a6737ab1f8
조윤정 <hiyun20031@gmail.com>
2022-11-18 16:48:52 +0900

    finish counter with vanilla redux

--- src/index.js
+++ src/index.js
@@ -4,20 +4,29 @@ const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-// const reducer = () => {};
-// const store = createStore(reducer);
+const PLUS = "PLUS";
+const MINUS = "MINUS";
 
-//naming
 const countModifier = (count = 0, action) => {
-  // console.log(count, action);
-  if (action.type === "PLUS") return count + 1;
-  else if (action.type === "MINUS") return count - 1;
-  else return count;
+  switch (action.type) {
+    case PLUS:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
 const countStore = createStore(countModifier);
 
-plus.addEventListener("click", ()=>{countStore.dispatch({type:"PLUS"})});
-minus.addEventListener("click", ()=>{countStore.dispatch({type:"MINUS"})});
+plus.addEventListener("click", () => {
+  countStore.dispatch({ type: PLUS });
+});
+minus.addEventListener("click", () => {
+  countStore.dispatch({ type: MINUS });
+});
 
 number.innerText = 0;
-countStore.subscribe(()=>{number.innerText = countStore.getState()});
+countStore.subscribe(() => {
+  number.innerText = countStore.getState();
+});
