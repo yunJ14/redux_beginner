f68ee6346f47e7d7aea3fc8756f716167d73d17d
조윤정 <hiyun20031@gmail.com>
2022-12-05 13:49:04 +0900

    createSlice 사용

--- src/store.js
+++ src/store.js
@@ -1,34 +1,33 @@
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
-
-export const addToDo = createAction("ADD", function prepare(text) {
-  return {
-    payload: {
-      text,
-      id: Date.now(),
-    },
-  };
-});
-export const deleteToDo = createAction("DELETE");
+import { configureStore, createSlice, current } from "@reduxjs/toolkit";
 
 const initialState = JSON.parse(localStorage.getItem("toDos"));
 
 initialState || localStorage.setItem("toDos", JSON.stringify([]));
 
-const reducer = createReducer(initialState, (builder) => {
-  builder
-    .addCase(addToDo, (state, action) => {
-      const newState = { text: action.payload.text, id: action.payload.id };
-      state && state.unshift(newState);
-      localStorage.setItem("toDos", JSON.stringify(state));
-    })
-    .addCase(deleteToDo, (state, action) => {
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState,
+  reducers: {
+    addToDo: {
+      reducer: (state, action) => {
+        const newState = { text: action.payload.text, id: action.payload.id };
+        current(state) && state.unshift(newState);
+        localStorage.setItem("toDos", JSON.stringify(state));
+      },
+      prepare: (text) => {
+        return { payload: { text, id: Date.now() } };
+      },
+    },
+    deleteToDo: (state, action) => {
       const deleteState = state.filter((toDo) => toDo.id !== action.payload);
       localStorage.setItem("toDos", JSON.stringify(deleteState));
       return deleteState;
-    })
-    .addDefaultCase((state, action) => state);
+    },
+  },
 });
 
-const store = configureStore({reducer});
+const store = configureStore({ reducer: toDos.reducer });
+
+export const { addToDo, deleteToDo } = toDos.actions;
 
 export default store;
